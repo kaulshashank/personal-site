333b6327abe03ee79d09b6dc5449cd61e905d265
Shashank Kaul <kaulshashank96@gmail.com>
2023-10-21 23:56:53 +0530

    Split App.tsx

---
+++ src/components.tsx
@@ -0,0 +1,42 @@
+import { css } from "@emotion/react";
+
+interface LinkButtonProps {
+  text: string;
+  icon: JSX.Element;
+  link: string;
+}
+
+export function LinkButton(props: LinkButtonProps) {
+  const { text, icon, link } = props;
+  return (
+    <div
+      css={css`
+        background-color: var(--white);
+        border-radius: var(--border-radius);
+
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        gap: 0.75rem;
+
+        padding: 0 2.5rem;
+
+        cursor: pointer;
+        &:hover {
+          background-color: var(--tertiary);
+          h4 {
+            color: var(--white);
+          }
+          transform: scale(1.05);
+        }
+        transition: 100ms transform ease-in-out;
+
+        box-shadow: var(--elevation-2);
+      `}
+      onClick={() => window.open(link)}
+    >
+      {icon}
+      <h4>{text}</h4>
+    </div>
+  );
+}
